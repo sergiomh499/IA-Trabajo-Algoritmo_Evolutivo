2e21079eadbda0246307f97f18a553fe57c8c394
Sergio Martinez <sergiomh499@gmail.com>
2020-11-23 17:11:58 +0100

    Inicial

---
+++ run.sh
@@ -0,0 +1,17 @@
+
+#/usr/bin/sh
+prog=gatest
+conf=GAconfig
+fecha=$(date '+%Y%m%d_%H%M%S')
+
+ date > ./resultados/results_$fecha.txt
+
+for i in  1 2 3 4 5
+do
+		echo Executing run $i of $prog with configuration file $conf 
+		./$prog >> ./resultados/results_$fecha.txt
+		echo "************************************" >> ./resultados/results_$fecha.txt
+		echo "------------------------------------" >> ./resultados/results_$fecha.txt
+		echo "************************************" >> ./resultados/results_$fecha.txt
+done
+
