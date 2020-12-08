5610f8b81d96159bb7983636ea8f1e604a1b5b5e
Sergio <sergiomh499@gmail.com>
2020-12-08 19:47:15 +0100

    Realizados pruebas definitivas

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ fecha=$(date '+%Y%m%d_%H%M%S')
 
  date > ./resultados/results_$fecha.txt
 
-for i in  1 2 3 4 5
+for i in  1 2 3 4 5 6 7 8 9 10
 do
 		echo Executing run $i of $prog with configuration file $conf 
 		./$prog >> ./resultados/results_$fecha.txt
