e1d47d47103ad8370108067a02b3b5cac0552c7d
Restyled.io <commits@restyled.io>
2020-12-08 18:50:58 +0000

    Restyled by shfmt

--- run.sh
+++ run.sh
@@ -1,17 +1,14 @@
-
 #/usr/bin/sh
 prog=gatest
 conf=GAconfig
 fecha=$(date '+%Y%m%d_%H%M%S')
 
- date > ./resultados/results_"$fecha".txt
+date >./resultados/results_"$fecha".txt
 
-for i in  1 2 3 4 5 6 7 8 9 10
-do
-		echo Executing run "$i" of "$prog" with configuration file "$conf" 
-		./"$prog" >> ./resultados/results_"$fecha".txt
-		echo "************************************" >> ./resultados/results_"$fecha".txt
-		echo "------------------------------------" >> ./resultados/results_"$fecha".txt
-		echo "************************************" >> ./resultados/results_"$fecha".txt
+for i in 1 2 3 4 5 6 7 8 9 10; do
+  echo Executing run "$i" of "$prog" with configuration file "$conf"
+  ./"$prog" >>./resultados/results_"$fecha".txt
+  echo "************************************" >>./resultados/results_"$fecha".txt
+  echo "------------------------------------" >>./resultados/results_"$fecha".txt
+  echo "************************************" >>./resultados/results_"$fecha".txt
 done
-
